9fba33e1ad57ca80dc1bcd57e636cf5c2f04cc8b
Ajay Kumar <kajayd36@gmail.com>
2022-09-13 19:14:43 +0530

    🎉 dev : Project Setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { BrowserRouter, Routes, Route, Navigate, NavigationType } from 'react-router-dom';
-import './App.css';
-import Index from "./Pages/Index";
+import Signup from 'Pages/Signup';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Allproduct from "./Pages/Allproduct";
 import Cart from "./Pages/Cart";
 import Checkout from "./Pages/Checkout";
+import Index from "./Pages/Index";
 import Login from "./Pages/Login";
 import Productdetails from "./Pages/Productdetails";
-import Signup from "./Pages/Signup";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/allproduct" element={<Allproduct />} />
-        <Route path="/productdetail" element={<Productdetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />  
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Index />} />
+				<Route path="/allproduct" element={<Allproduct />} />
+				<Route path="/productdetail" element={<Productdetails />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="/checkout" element={<Checkout />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,66 +1,63 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-
-
-import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import Header from '../Components/Header';
 
 class Login extends Component {
   render() {
     return (
       <>
-    <Header/>
-    <div className="container-fluid">
-      <div className="main-login-section">
-      <div className="login-section">
-        <div className="row ">
-          <div className="col-lg-7 login-form-column ">
-            <h2>Log in</h2>
-            <form className="main-form">
-             
-              <div className="mb-4 ">
-                <div className="input-icons">
-                  <i className="name-icon">
-                  </i>
-                <input type="text" className=" fc-l " id="exampleInputName" aria-describedby="emailHelp" placeholder="Name" required/>
-             
+        <Header />
+        <div className="container-fluid">
+          <div className="main-login-section">
+            <div className="login-section">
+              <div className="row ">
+                <div className="col-lg-7 login-form-column ">
+                  <h2>Log in</h2>
+                  <form className="main-form">
+
+                    <div className="mb-4 ">
+                      <div className="input-icons">
+                        <i className="name-icon">
+                        </i>
+                        <input type="text" className=" fc-l " id="exampleInputName" aria-describedby="emailHelp" placeholder="Name" required />
+
+                      </div>
+                    </div>
+                    <div className="mb-4 ">
+                      <div className="input-icons">
+                        <i className="phone-icon">
+                        </i>
+                        <input type="text" className=" fc-l " id="exampleInputName" aria-describedby="emailHelp" placeholder="Phone number/ mail id" required />
+
+                      </div>
+                    </div>
+                    <div className="mb-4 ">
+                      <div className="input-icons">
+                        <i className="otp-icon">
+                        </i>
+                        <input type="text" className=" fc-l " id="exampleInputName" aria-describedby="emailHelp" placeholder="OTP" required />
+
+                      </div>
+                    </div>
+
+                    <div className="submit-section">
+                      <button type="submit" className="">Login</button>
+                      <span> <Link to="/signup"> Create new account</Link></span>
+                    </div>
+
+                  </form>
+                </div>
+                <div className="col-lg-5 pb-5 lgn-img">
+                  <img src={process.env.PUBLIC_URL + "pictures/l-img.png"} alt="" />
+                </div>
               </div>
             </div>
-            <div className="mb-4 ">
-              <div className="input-icons">
-                <i className="phone-icon">
-                </i>
-              <input type="text" className=" fc-l " id="exampleInputName" aria-describedby="emailHelp" placeholder="Phone number/ mail id" required/>
-           
-            </div>
-          </div>
-          <div className="mb-4 ">
-            <div className="input-icons">
-              <i className="otp-icon">
-              </i>
-            <input type="text" className=" fc-l " id="exampleInputName" aria-describedby="emailHelp" placeholder="OTP" required/>
-         
           </div>
         </div>
-            
-             <div className="submit-section">
-              <button type="submit" className="">Login</button>
-              <span> <Link to="/signup"> Create new account</Link></span>
-             </div>
-              
-            </form>
-          </div>
-          <div className="col-lg-5 pb-5 lgn-img">
-          <img src={process.env.PUBLIC_URL+"pictures/l-img.png"} alt="" />
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-    
 
-      <Footer/>
+
+        <Footer />
       </>
     )
   }
