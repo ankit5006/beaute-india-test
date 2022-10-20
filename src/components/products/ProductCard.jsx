f1fe5536328412cf16c564e46180e014112145ff
Ajay Kumar <kajayd36@gmail.com>
2022-10-20 20:46:12 +0530

    📱 main : change design of product card

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,12 +1,35 @@
 import noImage from 'assets/images/no-image.jpg';
+import whishlist from 'assets/pictures/noun-love-4726271.svg';
+import cart from 'assets/pictures/Path-13479.svg';
+
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from "react-router-dom";
+import { add } from "store/cart/actions";
+import notification from 'utilities/notification';
 import { removeHtmlTags } from 'utilities/str';
 
 const ProductCard = ({ product }) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
     const coverImage = product?.image || noImage
     const onError = (e) => {
         e.target.src = noImage
+    }
+
+    const addToCart = () => {
+        const data = {
+            id: product?.id || 0,
+            product_id: product?.id || 0,
+            name: product?.name || '',
+            price: Number(product?.sale_price) > 0 ? Number(product?.sale_price) : Number(product?.normal_price),
+            quantity: 1,
+            // attributes: values.attributes,
+            model: product || {}
+        }
+        dispatch(add(data))
+        notification('success', 'Product Added To Cart');
     }
 
     return (
@@ -17,19 +40,29 @@ const ProductCard = ({ product }) => {
                         src={coverImage}
                         onError={onError}
                         alt=""
+                        onClick={e => navigate(`/products/${product?.id}`)}
+                        style={{ cursor: 'pointer' }}
                     />
-                    <h2 className="card-title mb-2">
-                        {product?.name}
+                    <h2 className="card-title mb-2" style={{}}>
+                        <Link to={`/products/${product?.id}`}>
+
+                            {product?.name}
+                        </Link>
                     </h2>
-                    <p>
+                    <p className='line-clamp'>
                         {removeHtmlTags(product?.short_description)}
                     </p>
                     <div className="price-buy pt-3 pb-3">
                         <div className="price">
                             Rs {product?.normal_price}
                         </div>
-                        <div>
-                            <Link to={`/products/${product?.id}`} className='pro-buy-btn'>Buy Now</Link>
+                        <div className='d-flex'>
+                            <button className='border-0 p-0'>
+                                <img src={whishlist} alt="" style={{ height: 18 }} />
+                            </button>
+                            <button className='border-0 p-0' onClick={addToCart}>
+                                <img src={cart} alt="" style={{ height: 18 }} />
+                            </button>
                         </div>
                     </div>
                 </div>
