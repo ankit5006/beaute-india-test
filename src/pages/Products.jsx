edc7a719475355260a8ddd42bcd34f75a0502a31
Darshan Prajapati <darshanprajapati@Darshans-MacBook-Air.local>
2023-02-15 21:05:22 +0530

    check status

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -37,7 +37,7 @@ const Products = () => {
   }, [currentPage,data]);
 
     {data.map((product, idx) => (
-    category_arr[product.category.name] = product.category.id
+    category_arr[product?.category?.name] = product?.category?.id
     
   ))}
 
